844af9857ec146119fe76eaedf8a25e28864f345
Alexandr Kolodkin <alexandr.kolodkin@gmail.com>
2019-07-14 20:25:05 +0300

    Fixed usage example Fixed README.md

--- examples/SimpleModbusSlave/SimpleModbusSlave.ino
+++ examples/SimpleModbusSlave/SimpleModbusSlave.ino
@@ -1,26 +1,24 @@
-/*
- * Copyright © 2011-2012 Stéphane Raimbault <stephane.raimbault@gmail.com>
- * Copyright © 2015 Alexandr Kolodkin <alexandr.kolodkin@gmail.com>
- * License ISC, see LICENSE for more details.
- */
+// Copyright © 2011-2019 Stéphane Raimbault <stephane.raimbault@gmail.com>
+// Copyright © 2015-2019 Alexandr Kolodkin <alexandr.kolodkin@gmail.com>
+// License ISC, see LICENSE for more details.
+
 
 #include <SimpleModbusSlave.h>
 
-/* Initialize the slave with the ID 1 */
-SimpleModbusSlave slave(1);
-/* Allocate a mapping of 10 values */
-uint16_t regs[10];
+SimpleModbusSlave slave(1);   // Initialize the slave with the ID 1
+uint16_t regs[10];            // Allocate a mapping of 10 values
 
 void setup() {
-    /* The transfer speed is set to 115200 bauds */
+    // Initialize the first register to have a value to read
+    regs[0] = 0x1234;
+
+    // The transfer speed is set to 115200 bauds
     slave.setup(115200);
 }
 
 void loop() {
-    /* Initialize the first register to have a value to read */
-    regs[0] = 0x1234;
-    /* Launch Modbus slave loop with:
-       - pointer to the mapping
-       - max values of mapping */
-    slave.loop(regs, 10);
+    // Launch Modbus slave loop with:
+    // - pointer to the mapping
+    // - max values of mapping
+    slave.loop(regs, sizeof(regs) / sizeof(regs[0]));
 }
